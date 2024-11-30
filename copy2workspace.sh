a080ef40f3cdac3e91d6016e198b4cbb17cc16a3
Zach Moolman <jzmmoolman@scapticom.co.za>
2024-11-30 14:22:34 -0700

    Fix Script

--- copy2workspace.sh
+++ copy2workspace.sh
@@ -1,5 +1,5 @@
 
 git fetch
 git reset --hard origin/main
-rm -rf ../vivado-risc-v/workspace/Rocket64x1
-cp -r Rocket64x1 ../vivado-risc-v/workspace/
+rm -rf ../vivado-risc-v/workspace/rocket64x1
+cp -r rocket64x1 ../vivado-risc-v/workspace/
